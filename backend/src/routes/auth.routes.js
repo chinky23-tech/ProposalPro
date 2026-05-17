e48d04cc4aa166df221f81c55e6b5d28cf646146
chinky23-tech <chinky0330@gmail.com>
2026-05-17 22:53:24 +0530

    update

---
+++ backend/src/routes/auth.routes.js
@@ -0,0 +1,53 @@
+import { Router } from "express";
+const router = Router();
+import { register, login, getMe } from "../controllers/auth.controller.js";
+import protect from "../middleware/auth.middleware.js";
+/**
+ * @openapi
+ * /api/auth/register:
+ *   post:
+ *     summary: Register a new user
+ *     tags:
+ *       - Auth
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: User registered successfully
+ */
+router.post("/register", register);
+/**
+ * @openapi
+ * /api/auth/login:
+ *   post:
+ *     summary: Authenticate a user and get a token
+ *     tags:
+ *       - Auth
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Login successful
+ */
+router.post("/login", login);
+export default router;
